bbd7ce3db8d95c5b07ceee9fef16f8e2f22ef04d
saidali77 <mamedovsaidali7@gmail.com>
2020-07-17 16:05:38 +0600

    test2

---
+++ task4.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+guests = ['Saidali', 'Abdulla', 'Imran']
+a = guests.pop()
+print(a)
+
+guests.insert(2, 'Aibek')
+
+print('Я приглашаю на обед ' + guests[0])
+print('Я приглашаю на обед ' + guests[1])
+print('Я приглашаю на обед ' + guests[2])
